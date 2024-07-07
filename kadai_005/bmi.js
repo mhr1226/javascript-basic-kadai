0630156caa003473a0d4e0b08184de1c7e1202a1
mhr1226 <156394244+mhr1226@users.noreply.github.com>
2024-07-07 21:40:07 +0900

    uploads kadai_005

---
+++ kadai_005/bmi.js
@@ -0,0 +1,20 @@
+let bodyWeight = 68;
+// 変数をletと捉えているかconstと捉えているかがわからなった為
+// 両方記述してます
+
+// const bodyWeight = 68;
+
+let height = 1.7;
+
+// const height = 1.7;
+
+let bmi = (bodyWeight)/(height*height);
+
+// const bmi = (bodyWeight)/(height*height);
+
+console.log(bmi);
+
+// 以下実験で書いただけなので気にしないでください
+// console.log(`BMI:${bodyWeight/(height*height)}`);
+
+// console.log(`BMI:${bmi}`);
